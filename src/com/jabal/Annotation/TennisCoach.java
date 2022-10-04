badb0cfc5dd8d8a435d8e59854b5bdcf41f8f76f
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-10-05 01:59:22 +0200

    Java Annotations - Bean Scopes and Lifecycle Methods closes #1

--- src/com/jabal/Annotation/TennisCoach.java
+++ src/com/jabal/Annotation/TennisCoach.java
@@ -1,5 +1,8 @@
 package com.jabal.Annotation;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -10,12 +13,20 @@ public class TennisCoach implements Coach {
 	@Autowired
 	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
-	
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
-
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");		
+	}
 	// define a setter method 
 	/*(setter injection)
 	@Autowired
