4e1ee53f53ff5cd96712d495360116ed3c589bb9
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-10-05 01:34:47 +0200

    first commit

---
+++ src/com/jabal/Annotation/DatabaseFortuneService.java
@@ -0,0 +1,13 @@
+package com.jabal.Annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/jabal/Annotation/FortuneService.java
@@ -0,0 +1,6 @@
+package com.jabal.Annotation;
+
+public interface FortuneService {
+	public String getFortune();
+	
+}
